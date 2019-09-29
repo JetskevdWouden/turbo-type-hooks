5fb69dea994297d662e207b2faf1fcd51326384d
JetskevdWouden <zegmaarjet@gmail.com>
2019-09-29 18:10:38 +0200

    game hook

--- src/App.js
+++ src/App.js
@@ -7,32 +7,52 @@ const App = () => {
     "The tide is high, but I'm holding on I'm gonna be your number one",
     "A scrub is a guy that thinks he's fly And is also known as a busta (busta)"
   ];
+
+  const initialGameState = {
+    victory: false,
+    startTime: null,
+    endTime: null
+  }
+
   const [snippet, setSnippet] = useState('');
   const [userText, setUserText] = useState('');
+  const [gameState, setGameState] = useState(initialGameState);
 
   const updateUserText = event => {
     setUserText(event.target.value);
-    console.log('current userText', userText);
+    // console.log('current userText', userText);
+    if(event.target.value === snippet) {
+      setGameState({
+        ...gameState,
+        victory: true,
+        endTime: new Date().getTime() - gameState.startTime
+      })
+    }
   }
 
   const chooseSnippet = snippetIndex => () => {
     console.log('setSnippet', snippetIndex);
     setSnippet(snippets[snippetIndex]);
+    setGameState({
+      ...gameState,
+      startTime: new Date().getTime()
+    })
   }
 
   return (
     <div className="App">
       <div className="game">
         <h2>TURBO TYPE</h2>
-        <hr />
+        <br />
         <h3>snippet</h3>
         {snippet}
+        <h4>{gameState.victory ? `Done! Time: ${gameState.endTime}ms` : null}</h4>
         <input value={userText} onChange={updateUserText} />
-        <hr />
+        <br />
         {
           snippets.map((snippet, index) => (
             <button onClick={chooseSnippet(index)} key={index}>
-              {snippet.substring(0,10)}...
+              {snippet.substring(0,50)}...
             </button>
           ))
         }

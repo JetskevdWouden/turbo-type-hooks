de5b81c8b7d33ec01bf8f83b6adbd622498e9be6
JetskevdWouden <zegmaarjet@gmail.com>
2019-09-29 16:44:37 +0200

    userText hook

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
 const App = () => {
+  const [userText, setUserText] = useState('');
+  const updateUserText = event => {
+    setUserText(event.target.value);
+    console.log('current userText', userText);
+  }
+
   return (
     <div className="App">
       <h2>TURBO TYPE</h2>
-      <input />
+      <input value={userText} onChange={updateUserText}/>
     </div>
   );
 }

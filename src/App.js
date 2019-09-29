d4d5d4c98756fb5e087d262f25a5087af92e772f
JetskevdWouden <zegmaarjet@gmail.com>
2019-09-29 17:06:23 +0200

    snippet hook

--- src/App.js
+++ src/App.js
@@ -2,16 +2,41 @@ import React, { useState } from 'react';
 import './App.css';
 
 const App = () => {
+  const snippets = [
+    'Oops, I did it again, oombap oombap',
+    "The tide is high, but I'm holding on I'm gonna be your number one",
+    "A scrub is a guy that thinks he's fly And is also known as a busta (busta)"
+  ];
+  const [snippet, setSnippet] = useState('');
   const [userText, setUserText] = useState('');
+
   const updateUserText = event => {
     setUserText(event.target.value);
     console.log('current userText', userText);
   }
 
+  const chooseSnippet = snippetIndex => () => {
+    console.log('setSnippet', snippetIndex);
+    setSnippet(snippets[snippetIndex]);
+  }
+
   return (
     <div className="App">
-      <h2>TURBO TYPE</h2>
-      <input value={userText} onChange={updateUserText}/>
+      <div className="game">
+        <h2>TURBO TYPE</h2>
+        <hr />
+        <h3>snippet</h3>
+        {snippet}
+        <input value={userText} onChange={updateUserText} />
+        <hr />
+        {
+          snippets.map((snippet, index) => (
+            <button onClick={chooseSnippet(index)} key={index}>
+              {snippet.substring(0,10)}...
+            </button>
+          ))
+        }
+      </div>
     </div>
   );
 }
